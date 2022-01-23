9866043ea74ba8675e7ee98a6b5abf34c29cabeb
jlbfb <38572799+jlbfb@users.noreply.github.com>
2022-01-23 14:26:36 -0700

    Added user profile feed api

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings # This retrieves settings from our django settings.py file
 
 
 # Create your models here.
@@ -56,3 +57,18 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         """Return string representation of user"""
         return self.email
 
+
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL, # This is so we can make the update in the settings.py file only
+        on_delete=models.CASCADE,
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as a string"""
+        return self.status_text
+
+        
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -3,11 +3,14 @@ from rest_framework.routers import DefaultRouter
 from profiles_api import views
 
 
+# This is how you register ViewSets
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
 
 
+# This is how typical views are set
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
     path('login/', views.UserLoginApiView.as_view()),
